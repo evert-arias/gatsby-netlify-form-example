5cacfd69559a1b8155f93f212257b2d91db871fd
Evert Arias <evert.arias@hotmail.com>
2020-03-26 13:08:13 -0400

    Update thanks page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,10 +1,28 @@
 import React from "react"
 import Layout from "../components/layout"
+import { makeStyles } from "@material-ui/core/styles"
+import { Alert, AlertTitle } from "@material-ui/lab"
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}))
 
 const Thanks = () => {
+  const classes = useStyles()
   return (
     <Layout>
-      <p>Your info has been submitted!</p>
+      <div className={classes.root}>
+        <Alert severity="success">
+          <AlertTitle>Success</AlertTitle>
+          Your info has been submitted!
+        </Alert>
+      </div>
     </Layout>
   )
 }
